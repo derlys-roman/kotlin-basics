88578ab0b302a8735f0ff433eeaabe1722942a01
Derlys <derlysr6@gmail.com>
2022-06-17 06:58:06 -0300

    add model and data class, refactor

---
+++ app/src/main/java/com/example/jogodavelha/model/Data.kt
@@ -0,0 +1,14 @@
+package com.example.jogodavelha.model
+
+class Data {
+    val gamelist = arrayListOf(
+        listOf(1, 2, 3),
+        listOf(4, 5, 6),
+        listOf(7, 8, 9),
+        listOf(1, 4, 7),
+        listOf(2, 5, 8),
+        listOf(3, 6, 9),
+        listOf(1, 5, 9),
+        listOf(3, 5, 7)
+    )
+}
---
+++ app/src/main/java/com/example/jogodavelha/viewModel/Model.kt
@@ -0,0 +1,5 @@
+package com.example.jogodavelha.viewModel
+
+class Model(private val model: Model) {
+
+}
